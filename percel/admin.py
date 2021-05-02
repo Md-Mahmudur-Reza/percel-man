b67e6abcfc463f698b311a877b3281ec716676e7
Mohammad <mahmudreza27@gmail.com>
2021-05-02 10:47:52 +0600

    created model

--- percel/admin.py
+++ percel/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Destination, Merchant, Product
 
-# Register your models here.
+
+admin.site.register(Destination)
+admin.site.register(Merchant)
+admin.site.register(Product)
+
--- percel/models.py
+++ percel/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Destination(models.Model):
+    destination = models.CharField(max_length=100)
+    def __str__(self):
+        return self.destination
+
+class Merchant(models.Model):
+    merchant_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    products = models.TextField()
+    total_weight = models.IntegerField()
+    destinations = models.ForeignKey(Destination, on_delete=models.CASCADE)
+    def __str__(self):
+        a= self.merchant_id
+        a = str(a)
+        return a
+
+
+class Product(models.Model):
+    percel_id = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+    product_type = models.CharField(max_length=100) 
+    def __str__(self):
+        return self.product_type
+
+
