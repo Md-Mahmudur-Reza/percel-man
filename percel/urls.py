5acc345f2907dee14f9fc39cc4005aba0bcf8871
Mohammad <mahmudreza27@gmail.com>
2021-05-04 10:23:33 +0600

    data can be fetched from DB

--- percel/urls.py
+++ percel/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('', include('django.contrib.auth.urls')),
     path('order/', views.order, name='order'),
+    path('invoice/', views.invoice, name='invoice')
 ]
--- percel/views.py
+++ percel/views.py
@@ -15,16 +15,37 @@ def index(request):
         return render(request, 'percel/index.html', {'merchantform':form})
 
 
+
 def  order(request):
     if request.method == 'POST':
         filled_form = ProductForms(request.POST)
         if filled_form.is_valid():
+            nam = filled_form.cleaned_data['percel_id']
+            PERCEL = Merchant.objects.get(name=nam)
+            PERCEL_NUMBER_PK = PERCEL.pk
+
+            model_obj = Merchant.objects.get(id = PERCEL_NUMBER_PK)
+
+            model_name = model_obj.name
+            model_products = model_obj.products
+            model_weight = model_obj.total_weight
+            model_destinations = model_obj.destinations
+
+            prod_type = filled_form.cleaned_data['product_type']
+
+            # PERCEL_NUMBER = filled_form.cleaned_data['percel_id']
             filled_form.save()
-            note = "%s's %s product will be delivered"%(filled_form.cleaned_data['percel_id'], filled_form.cleaned_data['product_type'])
-            new_form = ProductForms()
-            context = {'product_form':new_form,'note':note}
-            return render(request, 'percel/order.html', context)
+            #note = "%s's %s product will be delivered"%(filled_form.cleaned_data['percel_id'], filled_form.cleaned_data['product_type'])
+            # new_form = ProductForms()
+            #context = {'product_form':new_form}
+            # context = {'percel_number':PERCEL_NUMBER_PK, 'obj':model_obj}
+            context = {'name':model_name, 'product':model_products, 'weight': model_weight, 'destinations':model_destinations, 'prod_type':prod_type}
+            return render(request, 'percel/invoice.html', context)
     else:
         form = ProductForms()
         context = {'product_form':form}
         return render(request, 'percel/order.html', context)
+
+def invoice(request):
+    context = {'percel_number':PERCEL_NUMBER}
+    return render (request, 'percel/invoice.html', context)
